c2e23c3ea9d474d513b0155b997aa0499f546e8d
Eugene Godun <eugene.godun@gmail.com>
2014-05-05 17:51:28 +0300

    header

--- js/common.js
+++ js/common.js
@@ -1,3 +1,32 @@
 head.ready(function() {
-	console.log($('body').html());
+	$(document).click(function(){
+		$(".js-search").removeClass("is-active");
+	});  
+	$(".js-search-key").on("click", function(event){
+		$(this).parents(".js-search").addClass("is-active");
+		event.stopPropagation();
+	});
+	$(".js-search").on("click", function(event){
+		event.stopPropagation();
+	});
+	$(".js-search-clear").on("click", function(){
+		$(this).parents(".js-search").find(".js-search-text").val("");
+		return false;
+	});
+	$(".js-header-key").on("click", function(event){
+		$(".js-header-drop").toggleClass("is-active");
+		return false;
+	});
+	function fixedMenu() {
+        if ($(window).scrollTop() > 0) {
+            $("body").addClass("has-fixed-header");
+        }
+        else {
+             $("body").removeClass("has-fixed-header");
+        }
+    }
+    fixedMenu();
+    $(document).scroll(function(){
+        fixedMenu();
+    });
 });
