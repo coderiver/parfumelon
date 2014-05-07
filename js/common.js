4f92103aa90f29882fdcdc346368337fb4df7ee2
Eugene Godun <eugene.godun@gmail.com>
2014-05-07 13:31:00 +0300

    catalog_in

--- js/common.js
+++ js/common.js
@@ -60,5 +60,21 @@ head.ready(function() {
 		return false;
 	});
 
+	$(".js-gallery").each(function(){
+		var gallery = $(this);
+		var img = gallery.find(".js-gallery-img img");
+		var preview_first = gallery.find(".js-gallery-preview a").first();
+		var first_img = preview_first.attr("href");
+		preview_first.addClass("is-active");
+		img.attr("src", first_img);
+	});
+	$(".js-gallery-preview a").on("click", function(){
+		var img = $(this).attr("href");
+		$(".js-gallery-preview a").removeClass("is-active");
+		$(this).addClass("is-active");
+		$(this).parents(".js-gallery").find(".js-gallery-img img").attr("src", img)
+		return false;
+	});
+
 });
 
