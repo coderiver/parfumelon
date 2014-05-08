ae2404cfd091d5668fd140d938545d36b0638dce
Eugene Godun <eugene.godun@gmail.com>
2014-05-08 14:25:31 +0300

    all pages without popups

--- js/common.js
+++ js/common.js
@@ -76,5 +76,137 @@ head.ready(function() {
 		return false;
 	});
 
+	function choose() {
+        var number = $(".js-choose");
+        number.each(function(){
+            var max_number = +($(this).attr("data-max-number"));
+            var input = $(this).find("input");
+            var plus = $(this).find(".js-plus");
+            var minus = $(this).find(".js-minus");
+            plus.on("click", function(){
+                var val = +(input.val());
+                if (val >= max_number) {
+                    return false
+                }
+                else {
+                    val += 1;
+                    input.val(val);
+                }
+            });
+            minus.on("click", function(){
+                var val = +(input.val());
+                if (val > 1) {
+                    val -= 1;
+                    input.val(val);
+                }
+                else {
+                    return false;
+                }
+            });
+        });
+    }
+    choose();
+
+    $(".js-del-row").on("click", function(){
+    	$(this).parents("tr").addClass("is-remove-ready");
+    	setTimeout(function(){
+    		$("tr.is-remove-ready").remove();
+    	}, 200);
+    });
+
+    $(".js-validate-form").each(function(){
+		if ($(this).length > 0) {
+			$(this).validate({
+				errorClass: "has-error",
+				rules: {
+					firstname: "required",
+					lastname: "required",
+					username: {
+						required: true,
+						minlength: 2
+					},
+					password: {
+						required: true,
+						minlength: 5
+					},
+					confirm_password: {
+						required: true,
+						minlength: 5,
+						equalTo: "#password"
+					},
+					email: {
+						required: true,
+						email: true
+					},
+					tel: {
+						required: true,
+						minlength: 2,
+						phoneUS: true
+					},
+					address: {
+						required: false,
+						minlength: 2
+					},
+					message: {
+						minlength: 4
+					}
+				},
+				messages: {
+					firstname: "Вас так зовут?",
+					lastname: "У вас такая фамилия?",
+					// username: {
+					// 	required: "Please enter a username",
+					// 	minlength: "Your username must consist of at least 2 characters"
+					// },
+					password: {
+						required: "Пароли не совпадают",
+						minlength: "Минимум 5 символов",
+					},
+					confirm_password: {
+						required: "Пароли не совпадают",
+						minlength: "Минимум 5 символов",
+						equalTo: "Пароли не совпадают"
+					},
+					email: "Неверный формат",
+					address: "Это Ваш адрес?",
+					//tel: "Телефон с ошибкой :(",
+					tel: {
+						required: "Телефон с ошибкой",
+						phoneUS: "Please enter a valid phone number: (e.g. 19999999999 or 9999999999)"
+					},
+					message: {
+						required: "Это Ваш вопрос?",
+						minlength: "Это Ваш вопрос?"
+					}
+					//agree: "Please accept our policy"
+				}
+			});
+		}
+	});
+	$(".js-enter-form").each(function(){
+			if ($(this).length > 0) {
+				$(this).validate({
+					errorClass: "has-error",
+					rules: {
+						password: {
+							required: true,
+							minlength: 5
+						},
+						email: {
+							required: true,
+							email: true
+						},
+					},
+					messages: {
+						password: {
+							required: "Неверный пароль",
+							minlength: "Минимум 5 символов",
+						},
+						email: "Неверный формат",
+					}
+				});
+			}
+		});
+
 });
 
--- js/init.js
+++ js/init.js
@@ -1,1 +1,1 @@
-head.load("js/lib/jquery.js","js/lib/fotorama.js", "js/common.js");
+head.load("js/lib/jquery.js","js/lib/fotorama.js","js/lib/jquery.validate.js", "js/common.js");
